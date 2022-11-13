0b92759da81e650e9df9e7b5ba2801dfba677a36
Jason R. Clark <jason@jasonrclark.net>
2022-11-12 20:41:46 -0800

    Tidying and refactoring

--- code.js
+++ code.js
@@ -33,3 +33,16 @@ function chart({elementName, title, type, yAxisKey, data}) {
     }
   });
 }
+
+function vanity({elementName, value, unit}) {
+  const element = document.getElementById(elementName);
+  element.innerHTML = value.toFixed(2) + " " + unit;
+}
+
+function vanityDistance({elementName, value}) {
+  vanity({elementName, value, unit: 'miles'});
+}
+
+function vanitySpeed({elementName, value}) {
+  vanity({elementName, value, unit: 'mph'});
+}
